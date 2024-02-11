34fd0d5087858ceec7b7a4f9f8f2390f1cb98447
Tanmoy0077 <tanmoy.pradhan0077@gmail.com>
2024-02-11 17:02:55 +0530

    Updating the files and adding gitignore

--- App.py
+++ App.py
@@ -1,48 +1,32 @@
 import streamlit as st
 import YoutubeVideo as yt
+import os
 
 st.set_page_config(page_title="YouTube Downloader", layout="wide")
 st.title("YouTube Video Downloader")
-link = st.text_input("Enter the video link")
-q = st.sidebar.radio("Select the quality ", ["240p","360p", "480p", "720p", "1080p", "2160p"])
 
-# if st.button("Check"):
-#     if link == "":
-#         st.write("Please Provide link")
-#     else:
-#         try:
-#             if yt.VideoChecker(link, q):
-#                 st.write("Video Available")
-#             else:
-#                 st.write("Video Unavailable")
-#         except:
-#             st.write("Invalid Link")
+# UI component
+link = st.text_input("Please provide the video link")
+q = st.sidebar.radio("Select the quality ", ["144p", "240p","360p", "480p", "720p", "1080p",
+                                              "1440p", "2160p"])
 
+# Exixting Video removal
+if not os.path.exists("./out"):
+    os.mkdir("out")
+else:
+    try:
+        os.remove("./out/video.mp4")
+    except:
+        pass
+
+# Download system
 if st.button("Download"):
     if link == "":
         st.warning("Please Provide link")
     else:
-        try:
-            if yt.VideoChecker(link, q):
-                st.button("Get File",data=yt.Download(link, q))
-            else:
-                st.warning("Video Unavailable")
-        except:
-            st.error("Invalid Link")
-
-with st.form("Test form"):
-    link = st.text_input("This is inside form")
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        if link == "":
-            st.warning("Please Provide link")
+        if yt.VideoChecker(link, q):
+            yt.Download(link, q)
+            st.success("Your Video is found, download using the video menu")
+            st.video("./out/video.mp4")
         else:
-            try:
-                if yt.VideoChecker(link, q):
-                    yt.Download(link, q)
-                    st.success("Download Successfull")
-                else:
-                    st.warning("Video Unavailable")
-            except:
-                st.error("Invalid Link")
-    
+            st.error("Video Unavailable, Please try with different quality")
--- YoutubeVideo.py
+++ YoutubeVideo.py
@@ -1,27 +1,36 @@
 from pytube import YouTube
 
+# * For Checking the video quality
+
 def VideoChecker(link, quality):
     youtubeObject = YouTube(link)
-    streams = youtubeObject.streams.filter(resolution=quality, progressive=True)
+    try:
+        youtubeObject.check_availability()
+    except:
+        return False
+    
+    streams = youtubeObject.streams.filter(resolution=quality, progressive=True,file_extension='mp4')
     if len(streams) != 0:
         return True
     else:
         return False
-    
+
+# * For downloading a file
+        
 def Download(link, quality):
     youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.filter(resolution=quality, progressive=True)
-    return youtubeObject[0].download()
+    downloadObject = youtubeObject.streams.filter(resolution=quality, progressive=True,file_extension='mp4')
+    downloadObject[0].download(output_path="./out/", filename="video.mp4")
 
-
-def log_printer(youtubeObject):
-    for i in youtubeObject:
-        print(i)
-    
+# * Download using a list of links present inside a text file
+def myDownload(link, quality):
+    youtubeObject = YouTube(link)
+    downloadObject = youtubeObject.streams.filter(resolution=quality, progressive=True)
+    downloadObject[0].download()
 
 if __name__ == '__main__':
     file = open("links.txt", "r")
     for vid in file:
         print("Downloading ",vid)
-        Download(vid, "360p")
+        myDownload(vid, "360p")
         print("File downloaded")
